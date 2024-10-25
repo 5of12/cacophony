dec00c4fc6b901d559bed4a14508083df20a6689
Antony Nasce <nasce@mac.com>
2024-10-25 16:37:55 +0100

    Adding Double Trigger Action to facilitate Double Tap

---
+++ GestureSystem/Scripts/ActionDetection/DoubleTriggerAction.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+namespace Cacophony {
+    [CreateAssetMenu(menuName = "Cacophony/DoubleTriggerAction", fileName = "Double Trigger")]
+    
+    public class DoubleTriggerAction : IActionProcessor
+    {
+        private Vector3 currentPosition;
+        [Tooltip("If the user performs a successful Trigger within this time threshold, it will fire OnEnd.")]
+        [Range(0, 2)]
+        [SerializeField] private float _maxTimeToConsiderDoubleTap = 0.558f;
+        private float _timeOfLastTrigger = -1000f;
+        private bool _waitingForFirstTrigger = false;
+        private bool detecting = false;
+        
+        public override void Initialise(IDetectionSource detector)
+        {
+            base.Initialise();
+            detector.OnStart.AddListener( HandleStart );
+            
+            // There is no notion of 'Hold' for Double Trigger, it's two impulses...
+            //detector.OnHold.AddListener( HandleHold );
+            detector.OnEnd.AddListener( HandleEnd );
+            detector.OnCancel.AddListener( () => OnCancel?.Invoke() );
+        }
+        
+        public override void Evaluate(Vector3 position)
+        {
+            currentPosition = position;
+        }
+
+        private void OnStartTriggered()
+        {
+            float deltaTapTime = Time.time - _timeOfLastTrigger;
+            _timeOfLastTrigger = Time.time;
+
+            if (_waitingForFirstTrigger)
+            {
+                // Trigger #1
+                _waitingForFirstTrigger = false;
+                return;
+            }
+
+            if (deltaTapTime <= _maxTimeToConsiderDoubleTap && !_waitingForFirstTrigger)
+            {
+                // Trigger #2
+                OnEnd?.Invoke(new ActionEventArgs { position = currentPosition });
+                _waitingForFirstTrigger = true;
+                detecting = false;
+            }
+        }          
+        
+        private void HandleStart()
+        {
+            OnStart?.Invoke(new ActionEventArgs { position = currentPosition });
+            detecting = true;
+            OnStartTriggered();
+        }
+
+        private void HandleEnd()
+        {
+            if (detecting)
+            {
+                OnCancel?.Invoke();
+            }
+            detecting = false;
+        }
+    }
+}
