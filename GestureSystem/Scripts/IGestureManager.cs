2f70da9fdc641a0e87cf474af4b61a3356e40fda
Pete Nancollis <124496419+PeteNancollis@users.noreply.github.com>
2025-04-04 09:32:52 +0100

    Tooltips and default init on start to true

--- GestureSystem/Scripts/IGestureManager.cs
+++ GestureSystem/Scripts/IGestureManager.cs
@@ -5,14 +5,15 @@ namespace Cacophony
 {
     public abstract class IGestureManager<T> : MonoBehaviour
     {
+        [Tooltip("The gesture detector to use for this manager. Initialised on start only")]
         public IGestureDetector<T> gestureDetector;
+        [Tooltip("The action processor to use for this manager. Initialised on start only")]
         public IActionProcessor actionProcessor;
         [Tooltip("Initialise on start only if configuring from the inspector")]
-        public bool initOnStart = false;
+        public bool initOnStart = true;
 
         [Tooltip("Event to observe when the manager has completed initialisation")]
         public UnityEvent Ready;
-
         private bool ready = false;
 
         void Start()
